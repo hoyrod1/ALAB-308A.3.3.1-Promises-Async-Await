a73b80161f9d711a761cd3b3138f582a4c7e3d13
Rodney St Cloud <hoyrod1@aol.com>
2024-05-07 14:20:25 -0400

    Creating the differsnt ways of accessing the data with Promise.any, Promise.all, Promise.resolve(dbs.db1[id]),Async and await central(id), Async Promise.all, Async Promise.any([central(id)]), Async Promise.any([vault(id)])

--- src/index.mjs
+++ src/index.mjs
@@ -1,11 +1,60 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.mjs";
+// async function getUserData(id) {
+//   const dbs = {
+//     db1: db1,
+//     db2: db2,
+//     db3: db3,
+//   };
+//   try {
+//     let waiting = await dbs.db1(id);
+//     console.log(waiting);
+//   } catch {
+//     console.log(err);
+//   }
+//   console.log(dbs.db1(id));
+// Promise.any([dbs.db1(id), dbs.db2(id), dbs.db3(id)])
+//   .then((x) => {
+//     console.log(x);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// Promise.resolve(dbs.db1(id))
+//   .then((x) => {
+//     console.log(x);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// }
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3,
   };
+
+  // Promise.all([vault(id)])
+  //   .then((x) => {
+  //     console.log(x);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  let waiting = await central(id);
+
+  console.log(waiting);
+
+  // Promise.any([central(id)])
+  //   .then((x) => {
+  //     console.log(x);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 }
-console.log("It works");
+
+getUserData(8);
+// console.log("It works");
