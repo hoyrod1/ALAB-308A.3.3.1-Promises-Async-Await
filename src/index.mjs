7bef266cd1127ddb6a4ebcfecd5fe0e1f41fa839
Rodney St Cloud <hoyrod1@aol.com>
2024-05-09 23:01:41 -0400

    completed part 1: The scenario

--- src/index.mjs
+++ src/index.mjs
@@ -1,65 +1,131 @@
-//=============================================================//
+//=============================================================================//
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.mjs";
-//=============================================================//
+//=============================================================================//
 
-//=============================================================//
+//=============================================================================//
 async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3,
   };
-  //   let waiting = await central(id);
-  //   console.log(waiting);
 
   //--------------------------------------------------------------------------//
-  // This promise retrieves from the imported "vault" function
-  // with an object containing the user's name, email, address, and phone data.
-  Promise.all([vault(id)])
-    .then((x) => {
-      console.log("================== All Vault =====================");
-      console.log(x[0]);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  //--------------------------------------------------------------------------//
-  // This promise retrieves from any of the "dbs" function
-  // The user's username, website, and company data associated with the id
-  Promise.any([dbs.db1(id), dbs.db2(id), dbs.db3(id)])
-    .then((x) => {
-      console.log("================== Any dbs =====================");
-      console.log(x);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  //--------------------------------------------------------------------------//
-  Promise.resolve(dbs.db1(id))
-    .then((x) => {
-      console.log("================== Resolve dbs =====================");
-      console.log(x);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  let dbInUse = await central(id);
+  // console.log(dbInUse);
+  // dbs[dbInUse](id).then((x) => {
+  //   console.log(x);
+  // });
 
+  // const userVaultInfo = await vault(id);
+  // console.log(userVaultInfo);
   //--------------------------------------------------------------------------//
-  // This promise retrieves from the imported "central" function
-  // with the database associated with the id entered
-  Promise.any([central(id)])
+  Promise.all([vault(id), dbs[dbInUse](id)])
     .then((x) => {
-      console.log("================== Any Central =====================");
-      console.log(x);
+      console.log(
+        "========== Retrieve users data from the vault & dbs object using 'id' =========="
+      );
+      // return {
+      //   id: id,
+      //   name: x[0].name,
+      //   username: x[1].username,
+      //   email: x[0].email,
+      //   address: x[0].address,
+      //   phone: x[0].phone,
+      //   website: x[1].website,
+      //   company: x[1].company,
+      // };
+      console.log({
+        id: id,
+        name: x[0].name,
+        username: x[1].username,
+        email: x[0].email,
+        address: x[0].address,
+        phone: x[0].phone,
+        website: x[1].website,
+        company: x[1].company,
+      });
     })
     .catch((err) => {
       console.log(err);
     });
 }
-//=============================================================//
+//---------------------------------------------------------------------------//
+const userData = getUserData(1);
+// console.log(userData.then((user) => console.log(user)));
+//---------------------------------------------------------------------------//
 
-//=============================================================//
+//============================== Practice code ==============================//
+// async function getUserData(id) {
+//   const dbs = {
+//     db1: db1,
+//     db2: db2,
+//     db3: db3,
+//   };
+
+//--------------------------------------------------------------------------//
+// let dbInUse = await central(id);
+// console.log(dbInUse);
+// dbs[dbInUse](id).then((x) => {
+//   console.log(x);
+// });
+
+// const userVaultInfo = await vault(id);
+// console.log(userVaultInfo);
+//--------------------------------------------------------------------------//
+// This promise retrieves from the imported "vault" function
+// with an object containing the user's name, email, address, and phone data.
+// Promise.all([vault(id)])
+//   .then((x) => {
+//     console.log(
+//       "========== Retrieve users data from an object using 'all' =========="
+//     );
+//     console.log(x[0]);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+//--------------------------------------------------------------------------//
+// This promise retrieves from any of the "dbs" function
+// The user's username, website, and company data associated with the id
+// Promise.any([dbs.db1(id), dbs.db2(id), dbs.db3(id)])
+//   .then((x) => {
+//     console.log(
+//       "============= Retrieve users data from dbs using 'any' ============="
+//     );
+//     console.log(x);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+//--------------------------------------------------------------------------//
+// Promise.resolve(dbs.db1(id))
+//   .then((x) => {
+//     console.log("==== Retrieve users data from dbs using 'resolve' ====");
+//     console.log(x);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//--------------------------------------------------------------------------//
+// This promise retrieves from the imported "central" function
+// with the database associated with the id entered
+// Promise.any([central(id)])
+//   .then((x) => {
+//     console.log(
+//       "== Retrieves the database from the Central function using 'any' =="
+//     );
+//     console.log(x);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// }
+//============================================================================//
+
+//============================================================================//
 // function getUserData(id) {
 //   const dbs = {
 //     db1: db1,
@@ -124,5 +190,4 @@ async function getUserData(id) {
 //   });
 // }
 //=============================================================//
-getUserData(4);
 // console.log("It works");
